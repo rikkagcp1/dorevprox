6ad9a8237d5aef6e9fa9362056e007e5c2f7a2fe
rikkagcp1 <rikkagcp1@gmail.com>
2025-11-27 03:17:52 +1100

    Abandon Hibernation WebSocket API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { DurableObject } from "cloudflare:workers";
 import * as less from "./less"
 import * as utils from "./utils"
-import * as wsstream from "./wsstream"
-import { DuplexStreamFromWsStream, DuplexStreamFromWs } from "./stream"
+import { DuplexStreamFromWs } from "./stream"
 import { GlobalConfig, UUIDUsage } from "./config"
 
 const durableObjEndpoint = "/do";
@@ -60,60 +59,47 @@ function parseEnv(env: Env) : GlobalConfig {
 	};
 }
 
-function getDOFromWorkerEnv(env: Env) {
-	// Since we are hard coding the Durable Object ID by providing the constant name 'foo',
-	// all requests to this Worker will be sent to the same Durable Object instance.
-	return env.WEBSOCKET_HIBERNATION_SERVER.getByName("foo");
+function handleWs(request: Request, configProvider: () => GlobalConfig) {
+	const webSocketPair = new WebSocketPair();
+	const [client, server] = Object.values(webSocketPair);
+
+	const earlyDataHeader = request.headers.get('sec-websocket-protocol') || '';
+	const earlyDataParseResult = utils.base64ToUint8Array(earlyDataHeader);
+	if (!earlyDataParseResult.success) {
+		return new Response(null, { status: 500 });
+	}
+
+	server.accept();
+
+	const uuid = crypto.randomUUID();
+	const logPrefix = uuid.substring(0, 6);
+	const logger = utils.createLogger(logPrefix);
+	const globalConfig = configProvider();
+
+	const lessStream = DuplexStreamFromWs(server, earlyDataParseResult.data, logger);
+	less.handlelessRequest(lessStream, null, logger, globalConfig);
+
+	return new Response(null, {
+		status: 101,
+		webSocket: client,
+	});
 }
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 
 		if (url.pathname.startsWith(durableObjEndpoint)) {
 			// Let durable object handle the request
-			return getDOFromWorkerEnv(env).fetch(request);
+			return env.WEBSOCKET_HIBERNATION_SERVER.getByName("foo").fetch(request);
 		}
 
 		switch (request.method) {
 			case "GET":
 				const upgradeHeader = request.headers.get('Upgrade');
 				if (upgradeHeader && upgradeHeader === 'websocket') {
-					const webSocketPair = new WebSocketPair();
-					const [client, server] = Object.values(webSocketPair);
-
-					const earlyDataHeader = request.headers.get('sec-websocket-protocol') || '';
-					const earlyDataParseResult = utils.base64ToUint8Array(earlyDataHeader);
-					if (!earlyDataParseResult.success) {
-						return new Response(null, { status: 500 });
-					}
-
-					server.accept();
-
-					const uuid = crypto.randomUUID();
-					const logPrefix = uuid.substring(0, 6);
-					const logger = utils.createLogger(logPrefix);
 					const globalConfig = parseEnv(env);
-
-					const lessStream = DuplexStreamFromWs(server, earlyDataParseResult.data, logger);
-					less.handlelessRequest(lessStream, null, logger, globalConfig);
-
-					return new Response(null, {
-						status: 101,
-						webSocket: client,
-					});
+					return handleWs(request, () => globalConfig);
 				} else {
 					switch (url.pathname) {
 						case "/ip":
@@ -134,18 +120,9 @@ export default {
 	},
 };
 
-interface WebSocketConnection {
-	uuid: string,
-	enqueueChunk: (chunk: Uint8Array) => void,
-	onWsNormalClose: (closeInfo: wsstream.WebSocketCloseInfoLike) => void,
-	onWsUncleanClose: (closeInfo: wsstream.WebSocketCloseInfoLike) => void,
-}
-
 // Durable Object
 export class WebSocketHibernationServer extends DurableObject {
-	// Keeps track of all WebSocket connections
-	// When the DO hibernates, gets reconstructed in the constructor
-	sessions: Map<WebSocket, WebSocketConnection> = new Map();
+	// In-memory states
 	globalConfig: GlobalConfig;
 	bridgeContext = new less.BridgeContext(); // Tracks the reverse proxy states
 
@@ -153,25 +130,6 @@ export class WebSocketHibernationServer extends DurableObject {
 		super(ctx, env);
 
 		this.globalConfig = parseEnv(env);
-
-		// As part of constructing the Durable Object,
-		// we wake up any hibernating WebSockets and
-		// place them back in the `sessions` map.
-
-		// Get all WebSocket connections from the DO
-		/*
-		this.ctx.getWebSockets().forEach((ws) => {
-			let attachment = ws.deserializeAttachment();
-			if (attachment) {
-				// If we previously attached state to our WebSocket,
-				// let's add it to `sessions` map to restore the state of the connection.
-				this.sessions.set(ws, { ...attachment });
-			}
-		});
-		*/
-
-		// Sets an application level auto response that does not wake hibernated WebSockets.
-		// this.ctx.setWebSocketAutoResponse(new WebSocketRequestResponsePair('ping', 'pong'));
 	}
 
 	async fetch(request: Request): Promise<Response> {
@@ -181,7 +139,7 @@ export class WebSocketHibernationServer extends DurableObject {
 			case "GET":
 				const upgradeHeader = request.headers.get('Upgrade');
 				if (upgradeHeader && upgradeHeader === 'websocket') {
-					return this.handleWs(request);
+					return handleWs(request, () => this.globalConfig);
 				} else {
 					switch (pathname) {
 						case "/stats":
@@ -196,125 +154,5 @@ export class WebSocketHibernationServer extends DurableObject {
 		return new Response(null, {
 			status: 404,
 		});
-	}
-
-	async handleWs(request: Request): Promise<Response> {
-		// Creates two ends of a WebSocket connection.
-		const webSocketPair = new WebSocketPair();
-		const [client, server] = Object.values(webSocketPair);
-
-		// Calling `acceptWebSocket()` informs the runtime that this WebSocket is to begin terminating
-		// request within the Durable Object. It has the effect of "accepting" the connection,
-		// and allowing the WebSocket to send and receive messages.
-		// Unlike `ws.accept()`, `this.ctx.acceptWebSocket(ws)` informs the Workers Runtime that the WebSocket
-		// is "hibernatable", so the runtime does not need to pin this Durable Object to memory while
-		// the connection is open. During periods of inactivity, the Durable Object can be evicted
-		// from memory, but the WebSocket connection will remain open. If at some later point the
-		// WebSocket receives a message, the runtime will recreate the Durable Object
-		// (run the `constructor`) and deliver the message to the appropriate handler.
-		this.ctx.acceptWebSocket(server);
-
-		// Generate a random UUID for the session.
-		const uuid = crypto.randomUUID();
-		const logPrefix = uuid.substring(0, 6);
-		const logger = utils.createLogger(logPrefix);
-
-		// Attach the session ID to the WebSocket connection and serialize it.
-		// This is necessary to restore the state of the connection when the Durable Object wakes up.
-		// server.serializeAttachment({ id });
-		const earlyDataHeader = request.headers.get('sec-websocket-protocol') || '';
-		const earlyDataParseResult = utils.base64ToUint8Array(earlyDataHeader);
-		if (!earlyDataParseResult.success) {
-			return new Response(null, { status: 500 });
-		}
-
-		const {
-			resolve: onWsNormalClose,
-			reject: onWsUncleanClose,
-			promise: closedPromise,
-		} = utils.newPromiseWithHandle<wsstream.WebSocketCloseInfoLike>();
-
-		const readable = new ReadableStream<Uint8Array>({
-			start: (controller) => {
-				if (earlyDataParseResult.data && earlyDataParseResult.data.byteLength > 0)
-					controller.enqueue(earlyDataParseResult.data);
-
-				const WebSocketConnection: WebSocketConnection = {
-					uuid,
-					enqueueChunk: (chunk) => controller.enqueue(chunk),
-					onWsNormalClose,
-					onWsUncleanClose,
-				}
-
-				closedPromise.then((closeInfo) => logger("info", "WebSocketStream", `closed normally with code ${closeInfo.code}, reason: "${closeInfo.reason}"`));
-				closedPromise.catch(() => logger("info", "WebSocketStream", "closed with error"));
-				closedPromise.finally(() => controller.close());
-
-				// Add the WebSocket connection to the map of active sessions.
-				this.sessions.set(server, WebSocketConnection);
-			},
-			cancel: (reason) => {
-				server.close();
-			},
-		});
-
-		const writable = new WritableStream<Uint8Array>({
-			write: (chunk) => server.send(chunk),
-			close: () => {
-				// The server dont need to close the websocket first, as it will cause ERR_CONTENT_LENGTH_MISMATCH
-				// The client will close the connection anyway.
-				// TODO: Setup a timer to close the websocket after 10 seconds.
-				// logger("info", "WebSocketStream", "writable close()");
-				// server.close()
-			},
-		});
-
-		const openInfo: wsstream.WebSocketOpenInfoLike = {
-			protocol: "",
-			extensions: "",
-			readable,
-			writable,
-		};
-
-		const websocketStream: wsstream.WebSocketStreamLike = {
-			url: request.url,
-			opened: Promise.resolve(openInfo),
-			closed: closedPromise,
-			close: ({ code, reason }: wsstream.WebSocketCloseInfoLike = {}) => server.close(code, reason)
-		};
-
-		const lessStream = await DuplexStreamFromWsStream(websocketStream);
-		less.handlelessRequest(lessStream, this.bridgeContext, logger, this.globalConfig);
-
-		return new Response(null, {
-			status: 101,
-			webSocket: client,
-		});
-	}
-
-	async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
-		// Get the session associated with the WebSocket connection.
-		const session = this.sessions.get(ws)!;
-
-		if (typeof message == "string") {
-			message = new TextEncoder().encode(message);
-		}
-
-		session.enqueueChunk(new Uint8Array(message));
-	}
-
-	webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
-		// Get the session associated with the WebSocket connection.
-		const session = this.sessions.get(ws)!;
-
-		if (wasClean) {
-			session.onWsNormalClose({ code, reason });
-		} else {
-			session.onWsUncleanClose({ code, reason });
-		}
-		this.sessions.delete(ws);
-
-		// If the client closes the connection, the runtime will invoke the webSocketClose() handler.
-		// ws.close(code, 'Durable Object is closing WebSocket');
 	}
 }
